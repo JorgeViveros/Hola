d3f7b241ce3e21059154a30415ed8cf064bfb3d1
jorge armando viveros perez <jorge armando viveros perez>
2018-01-22 15:07:11 -0600

    'listen'

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+
+
+app.listen(process.env.PORT || 5000);
+
+/*
 app.listen(3000, function(){
 	console.log('App funcionando en el puerto 3000');
-})
+})*/
