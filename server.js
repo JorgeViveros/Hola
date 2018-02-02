b16a3289a137fd6bdd5c44fd18f0509442b41e64
JonathanNeyra <Linkin_Figure.11@Hotmail.com>
2018-02-02 13:15:06 -0600

    :fire:

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, function(){
+	console.log('http://localhost:5000');
+});
 
 /*
 app.listen(3000, function(){
